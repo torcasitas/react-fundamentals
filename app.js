f5a92594598b498867b3f99d439fffc0b8b03ab5
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-12 17:41:37 -0700

    jsx parentheses

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+//class App extends React.Component {
+//  render() {
+//    return <h1>Hello World</h1>;
+//  }
+//}
+
+// This will have state
+//class App extends React.Component {
+//  render() {
+//    return React.createElement('h1', null, 'Hello Guys');
+//  }
+//}
+
+// Stateless function
+//const App = () => { return <h1> Hello Boys </h1> }
+//const App = () => <h1> Hello Boys </h1>
+
+// jsx: wrapped dom elements in parentheses (more than one node)
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    );
   }
 }
 
