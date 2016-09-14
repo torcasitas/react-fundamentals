7019b36fdb57264137b14384b69245d6c76d341b
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-13 18:52:49 -0700

    using refs

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      txt: 'Joder que manera que caer agua',
-      cat: 0
+      red: 0,
+      green: 0,
+      blue: 0
     };
     this.update = this.update.bind(this);
   }
 
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    });
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        <hr/>
+        <Slider ref="red" update={this.update}  />
+        {this.state.red}
+        <br/>
+        <hr/>
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br/>
+        <hr/>
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br/>
+        <hr/>
       </div>
-    )
+    );
   }
 }
 
-// Stateless component
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update}/>
-      <h1>{props.txt}</h1>
-    </div>
-  )
+class Slider extends React.Component {
+  render() {
+    return (
+      <input type="range"
+        min="0"
+        max="255"
+        onChange={this.props.update} />
+    );
+  }
 }
 
 export default App;
