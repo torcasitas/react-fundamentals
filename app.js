a29fdd23fec521f3e95d48f28d2292e3496af0c4
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-13 18:29:21 -0700

    passing props from parent to child component

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class App extends React.Component {
       txt: 'Joder que manera que caer agua',
       cat: 0
     };
+    this.update = this.update.bind(this);
   }
 
   update(e) {
@@ -17,11 +18,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     )
   }
+}
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update}/>
+      <h1>{props.txt}</h1>
+    </div>
+  )
 }
 
 export default App;
