1e17b834f11f2ae030f512be39a17e8f3f6915da
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-13 19:08:59 -0700

    accessing props children values, i.e. innerHTML

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
-    this.update = this.update.bind(this);
-  }
-
-  update(e) {
-    let refs = this.refs;
-    this.setState({
-      red: ReactDOM.findDOMNode(refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(refs.blue.refs.inp).value
-    });
-  }
-
   render() {
-    return (
-      <div>
-        <hr/>
-        <Slider ref="red" update={this.update}  />
-        {this.state.red}
-        <br/>
-        <hr/>
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br/>
-        <hr/>
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br/>
-        <hr/>
-      </div>
-    );
+    return <Button> <Heart/> React</Button>
   }
 }
 
-class Slider extends React.Component {
+class Button extends React.Component {
   render() {
-    return (
-      <div>
-      <input ref="inp" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update} />
-      </div>
-    );
+    return <button>{this.props.children}</button>
   }
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default App;
