d66c060ec45ecc8c9aca119cfc349ec9f5ad6e16
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-13 18:21:51 -0700

    update component state

--- app.js
+++ app.js
@@ -1,45 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-//class App extends React.Component {
-//  render() {
-//    return <h1>Hello World</h1>;
-//  }
-//}
-
-// This will have state
-//class App extends React.Component {
-//  render() {
-//    return React.createElement('h1', null, 'Hello Guys');
-//  }
-//}
-
-// Stateless function
-//const App = () => { return <h1> Hello Boys </h1> }
-//const App = () => <h1> Hello Boys </h1>
-
-// jsx: wrapped dom elements in parentheses (more than one node)
 class App extends React.Component {
-  
+
+  constructor() {
+    super();
+    this.state = {
+      txt: 'Joder que manera que caer agua',
+      cat: 0
+    };
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
 
   render() {
-    let val = this.props.txt;
-    return <h1>{val}</h1>;
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'Que manera de caer agua'
-}
-
-ReactDOM.render(
-  <App cat={53}  />,
-  document.getElementById('app')
-)
 
 export default App;
