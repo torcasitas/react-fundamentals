a3ff1279cabd3698484e94b60f04b18a60f0aa2a
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-16 09:44:19 -0400

    testing react autosuggest

--- app.js
+++ app.js
@@ -1,61 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Autosuggest from 'react-autosuggest';
+import countryList from './countryList';
+
+// / Teach Autosuggest how to calculate suggestions for any given input value.
+  function getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : countryList.filter(lang =>
+      lang.name.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  }
+
+// When suggestion is clicked, Autosuggest needs to populate the input field
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+  function getSuggestionValue(suggestion) {
+    return suggestion.name;
+  }
+
+// Use your imagination to render suggestions.
+  function renderSuggestion(suggestion) {
+    return (
+      <span>{suggestion.name}</span>
+    );
+}
 
 class App extends React.Component {
+
   constructor() {
     super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
     this.state = {
-      val: 0
-    }
-    this.update = this.update.bind(this);
+      value: '',
+      suggestions: []
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
   }
 
-  update(e) {
-    this.setState({val: this.state.val + 1})
-  }
+  onChange (event, { newValue })  {
+    this.setState({
+      value: newValue
+    });
+  };
 
-  componentWillMount() {
-    console.log('mounting');
-  }
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested ({ value })  {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested()  {
+    this.setState({
+      suggestions: []
+    });
+  };
 
   render() {
-    console.log('rendering')
-    return <button onClick={this.update}>{this.state.val}</button>
-  }
+    const { value, suggestions } = this.state;
 
-  componentDidMount() {
-    console.log('Mounted')
-  }
+    // Autosuggest will pass through all these props to the input field.
+    const inputProps = {
+      placeholder: 'Type a country',
+      value,
+      onChange: this.onChange
+    };
 
-  componentWillUnmount() {
-    console.log('Adios Motherfucker!');
-  }
-
-}
-
-class Wrapper extends React.Component {
-
-  constructor(){
-    super();
-  }
-
-  mount() {
-    ReactDOM.render(<App />, document.getElementById('aContainer'))
-  }
-
-  unmount() {
-    ReactDOM.unmountComponentAtNode(document.getElementById('aContainer'))
-  }
-
-  render() {
+    // Finally, render it!
     return (
-      <div>
-        <button onClick={this.mount.bind(this)}>Mount</button>
-        <button onClick={this.unmount.bind(this)}>Unmount</button>
-        <div id="aContainer"></div>
-      </div>
-    )
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} />
+    );
   }
 }
 
-export default Wrapper;
+
+export default App;
