32022a19ab3669a28d8d4a97cdefd1bbfcfe73cb
odw566 <carlos.victoricaorozco@capitalone.com>
2016-12-22 12:16:27 -0800

    adding a filter to dynamically generated component

--- app.js
+++ app.js
@@ -13,14 +13,28 @@ class App extends React.Component {
       .then( ({results: items}) => this.setState({items}))
   }
 
+  filter (e) {
+    this.setState({filter: e.target.value})
+  }
+
   render() {
     let items = this.state.items
+
+    if (this.state.filter) {
+      items = items.filter( item => 
+        item.name.toLowerCase()
+        .includes(this.state.filter.toLowerCase())
+        )
+    }
+
     return (
       <div>
-        {items.map(item => <h4> {item.name} </h4>)}
+        <input type="text" onChange={this.filter.bind(this)} />
+        {items.map( item => <Person key={item.name} person={item} /> )}
       </div>
     )
   }
 }
 
+const Person = (props) => <h4>{props.person.name}</h4>
 export default App;
