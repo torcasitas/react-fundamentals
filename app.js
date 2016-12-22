84b14fb1df9e0c6fc15d83ecff5b0a394adda5ff
odw566 <carlos.victoricaorozco@capitalone.com>
2016-12-22 10:56:53 -0800

    use map to create components from arrays of data

--- app.js
+++ app.js
@@ -4,37 +4,23 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {data: [
-      {id:1, name: "Simon Bailey"}, {id: 2, name: "Thomas Burleson"},
-      {id:3, name: "Bailey Calahan"}, {id: 4, name: "Pepe Perron"},
-      {id:5, name: "Juan Camaney"}, {id: 6, name: "Aquiles Baeza Quieta"},
-      {id:7, name: "Poncho Pilatos"}, {id: 8, name: "Alma Maria Calvo"},
-      {id:9, name: "Tela Dejuiré"}, {id: 10, name: "Medas Elhoyon"},
-      {id:11, name: "Telas Poncho"}, {id: 12, name: "Kika Briones"}
-    ]};
+    this.state = {items: []};
+  }
+
+  componentWillMount () {
+    fetch( 'https://swapi.co/api/people/?format=json' )
+      .then( response => response.json() )
+      .then( ({results: items}) => this.setState({items}))
   }
 
   render() {
-    let rows = this.state.data.map( person => {
-      return <PersonRow key={person.id } data={person} />
-    })
+    let items = this.state.items
     return (
-      <table>
-        <tbody>{rows}</tbody>
-      </table>
+      <div>
+        {items.map(item => <h4> {item.name} </h4>)}
+      </div>
     )
   }
-
 }
-
-const PersonRow = (props) => {
-  return (
-    <tr>
-      <td>{props.data.id}</td>
-      <td>{props.data.name}</td>
-    </tr>
-  )
-}
-
 
 export default App;
